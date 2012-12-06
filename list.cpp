9f940739d6774d99b1fd543f7f4e33d8161a3164
jgutowsk <jgutowsk@unca.edu>
2012-12-06 15:29:06 -0500

    finished Naughty or Nice, everything appears to be working

--- list.cpp
+++ list.cpp
@@ -1,16 +1,59 @@
-#include "Entry.h"
+#include <cstdlib>
+#include <fstream>
 #include <iostream>
+#include <map>
+#include <string>
 
 using std::cout;
 using std::endl;
+using std::string;
+using std::map;
+using std::pair;
+using std::ifstream;
 
-int main() {
-  Entry<string> s("hello", "world");
+int main(int argc, char** argv) {
+  map<string, int> list;
+  string name;
+  map<string, int>::iterator it;
 
-  cout << s.getKey() << endl;
-  cout << s.getValue() << endl;
-  s.setValue("goodbye");
-  cout << s.getValue() << endl;
+  if(argc != 3) {
+    cout << "Usage: list [filename] [threshold]" << endl;
+  }
+  else {
+    cout << "filename: " << argv[1] << endl;
+    cout << "threshold: " << argv[2] << endl;
 
+    ifstream file(argv[1]);
+    if(file.is_open()) {
+      while(file.good()) {
+	getline(file, name);
+	if(list.count(name)) {
+	  list[name] = list[name]+1;
+	}
+	else {
+	  list.insert(pair<string, int>(name, 1));
+	}
+      }
+    }
+  
+
+    cout << endl;
+    cout << endl;
+    cout << "----NICE----" << endl;
+    for(it=list.begin(); it != list.end(); it++) {
+      if((*it).second >= atoi(argv[2])) {
+	cout << (*it).first << endl;
+      }
+    }
+
+    cout << endl;
+    cout << endl;
+    cout << "----NAUGHTY----" << endl;
+    for(it=list.begin(); it != list.end(); it++) {
+      if((*it).second < atoi(argv[2])) {
+	cout << (*it).first << endl;
+      }
+    }
+  }
   return 0;
 }
